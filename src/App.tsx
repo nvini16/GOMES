50ec4082e1b7ce239861280d976e390c21d41bc3
Nathanael <nathanael.vinicius03@gmail.com>
2026-09-09 09:38:01 -0300

    created: components

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+import { FirstComponent } from "./shared/components/FirstComponent";
+
+
+export default function App() {
+  return(
+    <FirstComponent />
+  )
+}
---
+++ src/shared/components/FirstComponent.tsx
@@ -0,0 +1,10 @@
+import { View, Text} from 'react-native';
+
+
+export const FirstComponent = () => {
+    return(
+        <View>
+            <Text>FirstComponent</Text>
+        </View>
+    );
+}
